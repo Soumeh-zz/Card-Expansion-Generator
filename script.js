ce96ded8ef7a0eb3922ba9494a345da109c552c9
Soumeh <stefi.brezovac@gmail.com>
2021-09-08 23:31:23 +0200

    working version commit

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function main() {
 
 function addCard() {
   let div = document.createElement('div');
-  div.classList.add("card");
 
   let img = document.createElement('img');
   div.appendChild(img);
@@ -13,6 +12,7 @@ function addCard() {
   name.placeholder = 'name';
   let imageURL = document.createElement('input');
   imageURL.placeholder = 'imageURL';
+  imageURL.onchange = function() { this.parentElement.getElementsByTagName('img')[0].src = this.value };
   let description = document.createElement('input');
   description.placeholder = 'description';
   div.appendChild(name);
@@ -41,5 +41,30 @@ function addCard() {
 }
 
 function downloadJSON(element) {
-  // do shit
+  const cards = document.getElementById('container').getElementsByTagName('div');
+  let json = []
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    const card_data = {
+      'name': card.getElementsByTagName('input')[0].value,
+      'imageURL': card.getElementsByTagName('input')[1].value,
+      'description': card.getElementsByTagName('input')[2].value,
+      'rarity': card.getElementsByTagName('select')[0].value,
+      'hp': card.getElementsByTagName('input')[3].value,
+      'atk': card.getElementsByTagName('input')[4].value
+    };
+    json.push(card_data);
+  }
+
+  if (json.length) {
+    const text = JSON.stringify(json, null, 2);
+    let download = document.createElement('a');
+    download.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(text);
+    download.setAttribute('download', 'cards.json');
+    download.style.display = 'none';
+
+    document.body.appendChild(download);
+    download.click();
+    document.body.removeChild(download);
+  }
 }
